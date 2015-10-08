c78a7cab6cacd54181eefa18e2bb49c28c968416
Lawrence Moore <lawrence2978@hotmail.com>
2015-10-08 12:39:38 -0400

    Added event listeners to nav buttons

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,3 +1,37 @@
+var myApp = {
+
+    //Initializes script
+    init: function(){
+        myApp.navEventListener();
+    },
+
+    //Puts nav buttons into an array
+    getNavElements: function(){
+        var navArray = document.getElementsByClassName('nav-area');
+        return navArray
+    },
+
+    //Adds event listeners to the nav array
+    navEventListener: function(){
+        var navArray = myApp.getNavElements(),
+            i;
+
+        for(i = 0; i < navArray.length; ++i){
+            navArray[i].addEventListener('click', function(){
+                myApp.openNav(this);
+            });
+        }
+    },
+
+    //Function for opening the nav
+    openNav: function(navElements){
+        console.log(navElements)
+    }
+};
+
+
+myApp.init();
+
 //var myDiv = document.getElementsByClassName('main-container');
 //
 //myDiv[0].addEventListener('click', function(){
