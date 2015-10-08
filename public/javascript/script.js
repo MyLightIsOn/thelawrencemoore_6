1f67815d84f96fe992c3132f276e1eb0dc5657ce
Lawrence Moore <lawrence2978@hotmail.com>
2015-10-08 14:42:29 -0400

    Updated package.json for Bower

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -44,9 +44,3 @@ var myApp = {
 
 
 myApp.init();
-
-//var myDiv = document.getElementsByClassName('main-container');
-//
-//myDiv[0].addEventListener('click', function(){
-//    Velocity(myDiv[0], { left: 900 })
-//});
